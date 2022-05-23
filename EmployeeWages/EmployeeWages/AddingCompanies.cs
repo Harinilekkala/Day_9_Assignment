efe1607aafdb369419513f1d6d2a37bf81091c8d
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-23 19:27:19 +0530

    [Harini]: program for present or absent

---
+++ EmployeeWages/EmployeeWages/AddingCompanies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class AddingCompanies
+    
+
+        {
+        public void AttendanceCheck()
+        {
+            Random value = new Random();
+            int randomCheck = value.Next(2);
+
+            switch (randomCheck)
+            {
+                case 0:
+                    {
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Employee is Present");
+                        break;
+                    }
+            }
+        }
+   }
+
+    }
---
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Employee
+{
+    internal class Employee
+    {
+        public static void Main(String[] args)
+        {
+            EmployeeWages.AddingCompanies getMethod = new EmployeeWages.AddingCompanies();
+            getMethod.AttendanceCheck();
+        }
+    }
+}
