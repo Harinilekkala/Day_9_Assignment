b4a6fa2042558732b23dcfc65780b7efcb716873
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-23 19:46:30 +0530

    [Harini]: check PartTime

--- EmployeeWages/EmployeeWages/AddingCompanies.cs
+++ EmployeeWages/EmployeeWages/AddingCompanies.cs
@@ -14,24 +14,33 @@ namespace EmployeeWages
         {
             int empWagePerHour = 20, empHours, dailyWage;
             Random value = new Random();
-            int randomCheck = value.Next(2);
+            int randomCheck = value.Next(3);
+            Console.WriteLine("Generated random value is:" + randomCheck);
 
             switch (randomCheck)
             {
-                case 0:
+                case 1:
+                    {
+                        Console.WriteLine("Employee is working Part Time");
+                        empHours = 4;
+                        dailyWage = empHours * empWagePerHour;
+                        Console.WriteLine("Employee wage is:" + dailyWage);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Employee is working Full Time");
+                        empHours = 8;
+                        dailyWage = empHours * empWagePerHour;
+                        Console.WriteLine("Employee wage is:" + dailyWage);
+                        break;
+                    }
+                default:
                     {
                         Console.WriteLine("Employee is Absent");
                         empHours = 0;
                         dailyWage = empHours * empWagePerHour;
-                        Console.WriteLine("Employee dailywage is:" + dailyWage);
-                        break;
-                    }
-                case 1:
-                    {
-                        Console.WriteLine("Employee is Present");
-                        empHours = 8;
-                        dailyWage = empHours * empWagePerHour;
-                        Console.WriteLine("Employee dailywage is:" + dailyWage);
+                        Console.WriteLine("Employee wage is:" + dailyWage);
                         break;
                     }
             }
